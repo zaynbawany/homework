4b0e9e6d19196d217cbeb899e58a5e86d1e6e080
zbawany <zayn.bawany@gmail.com>
2024-09-14 16:31:03 +0500

    commit

---
+++ agecounter.py
@@ -0,0 +1,9 @@
+age=int(input("enter your age"))
+if age>=20:
+   print("your age is older than 10-20")
+if age<20 and age>10:
+   print("your age is in between10-20")
+if age>=10 and age<20:
+   print("your age is in between 10-20")
+if age<10:
+   print("your age is younger than 10-20")
