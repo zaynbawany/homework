a62b15881fefe4f84f47a5e0c0baf8059b4e2fe4
zbawany <zayn.bawany@gmail.com>
2024-09-14 16:19:16 +0500

    commit

---
+++ square.py
@@ -0,0 +1,12 @@
+import turtle
+a=turtle.bgcolor("black")
+turtle.pendown
+turtle.pencolor("white")
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
