bd1d1a5999ee982b387ee35cae6fc45418e296e0
zbawany <zayn.bawany@gmail.com>
2024-09-14 13:58:19 +0500

    c

---
+++ 2.py
@@ -0,0 +1,5 @@
+lenght=int(input("enter the lenght of the cube "))
+bredth=int(input("enter the breadth of the cube "))
+height=int(input("enter the height of the cube "))
+cube=lenght*bredth*height
+print("the area of the cube is",cube)
